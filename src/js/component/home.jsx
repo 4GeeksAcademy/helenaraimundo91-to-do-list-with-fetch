6c8cab53838958a7ad23b2e215f2b8c149aeadba
helenaraimundo91 <139161478+helenaraimundo91@users.noreply.github.com>
2023-11-15 20:21:44 +0000

    css missing

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
 	const [todos, setTodos] = useState ([]);
 
 	return (
-		<div className="container">
+		<div className="center">
 			<h1>TODOS</h1>
 			<ul>
 				<li>
@@ -13,32 +13,31 @@ const Home = () => {
 						type="text"
 						onChange={(e) => setInputValue(e.target.value)}
 						value={inputValue}
-						onkeyPress={(e) => {
-						if (e.key === "enter") {
-							setTodos(todos.concat([inputValue]))}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								setTodos(todos.concat([inputValue]))
+							}
 						}}
 						placeholder="Things you need to do!"></input>
 				</li>
 				{todos.map((item, index) => (
-				<li>	
-					{item}{" "}
-					<i
-						class="fas fa-trash-alt"
-						onClick={() =>
-							setTodos(
-								todos.filter(
-									(t, currentIndex) =>
-									index != currentIndex
+					<li>	
+						{item}{" "}
+						<i
+							class="fas fa-trash-alt"
+							onClick={() =>
+								setTodos(
+									todos.filter(
+										(t, currentIndex) =>
+										index != currentIndex
 									)
-					)}></i>
-				</li>
+							)}></i>
+					</li>
 				))}
 			</ul>
 			<div>{todos.length} tasks</div>
 		</div>
 	)
-
-	}
-
+}
 
 export default Home;
